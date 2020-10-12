13a407f4f07bbc4ea3aa4c7b8d4e58becc46683c
ManaliParab12 <manaliparab10@gmail.com>
2020-10-12 16:31:39 +0530

    Compute Addition And Multiplication

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -3,7 +3,9 @@
 echo "Welcome to Arithmetic Computation And Sorting"
 
 
-read -p "Enter First Input" num1
-read -p "Enter Second Input" num2
-read -p "Enter Third Input" num3
+read -p "Enter First Input : " num1
+read -p "Enter Second Input : " num2
+read -p "Enter Third Input : " num3
 
+compute=`echo "scale=2; $num1+$num2*$num3" | bc`
+echo "Result : " $compute
