ca1d86f3e54817f4d9898ebba4f08809b621f62b
ManaliParab12 <manaliparab10@gmail.com>
2020-10-13 13:21:09 +0530

    Sort Computation Result in Ascending Order

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -57,5 +57,23 @@ done
 echo "Result in Descending Order : " ${arr[@]}
 }
 
+function Ascending() {
+for (( i=0; i<$size-1; i++ ))
+do
+   for (( j=0; j<$size-$i-1; j++ ))
+   do
+     if [[ ${arr[$j]} -gt ${arr[$j+1]} ]]
+     then
+        temp=${arr[$j]}
+        arr[j]=${arr[$j+1]}
+        arr[j+1]=$temp
+     fi
+   done
+done
+
+echo "Result in Ascending Order : " ${arr[@]}
+}
+
 Descending
+Ascending
 
